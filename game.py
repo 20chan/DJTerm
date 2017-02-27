6703fd492f213ba34926adfde86f51f73452a7fa
philly <adnim@mycodeis.sexy>
2017-02-28 03:48:10 +0900

    Change clear screen

--- game.py
+++ game.py
@@ -5,26 +5,28 @@ import os
 Note = collections.namedtuple('Note', ['time', 'pos'])
 Song = collections.namedtuple('Song', ['mp3', 'notes'])
 
+screen_size = os.popen('stty size', 'r').read().split()
+screen_size = [int(x) for x in screen_size][::1]
+
 def write_at(x, y, text):
     sys.stdout.write('\x1b7\x1b[%d;%df%s\x1b8' % (y, x, text))
     sys.stdout.flush()
 
 class Game:
     def __init__(self):
-        self.load_songs()
-        
+        self.load_songs() 
+        self.x, self.y, self.width, self.height = 0, 0, 30, screen_size[1] - 5
     def load_songs(self):
         pass
 
     def clear_screen(self):
-        os.system('clear')
+        # os.system('clear')
+        print('\n' * screen_size[1])
 
     def draw_board(self):
-        x, y, width, height = 0, 0, 30, 40
-        
-        for cur_y in range(y+1, y+height+2):
-            write_at(x+1, cur_y, '|')
-            write_at(x+width+1, cur_y, '|')
+        for cur_y in range(self.y+1, self.y+self.height+2):
+            write_at(self.x+1, cur_y, '|')
+            write_at(self.x+self.width+1, cur_y, '|')
 
 game = Game()
 game.clear_screen()
